408f40c10348dc7113aa7f69e6548415212f7358
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2025-02-09 13:30:30 +0530

    Lab Work

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+extern int yylex();
+extern FILE *yyin;
+
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+    {
+        yyin = fopen(argv[1], "r");
+        if (!yyin)
+        {
+            perror("Could not open input file");
+            return 1;
+        }
+    }
+    yylex();
+    return 0;
+}
